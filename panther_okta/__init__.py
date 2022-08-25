d726b581d8520ed92845e0782abad8b85f237cc8
Nic Martin <nicolas.martin@runpanther.io>
2022-08-25 10:53:12 -0500

    tweaks

--- panther_okta/__init__.py
+++ panther_okta/__init__.py
@@ -1,16 +1,12 @@
 from . import rules, sample_logs
-from _shared import *
-
-__all__ = ["use_all_with_defaults", "rules", "sample_logs"] + _shared.__all__
+from ._shared import *
 
 
 def use_all_with_defaults() -> None:
     rules.admin_disabled_mfa()
     rules.admin_role_assigned()
     rules.api_key_created()
-
     rules.brute_force_logins()
-
-    # rules.geo_improbable_access()
     rules.account_support_access()
     rules.support_reset()
+    # rules.geo_improbable_access()
